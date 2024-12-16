777d14180d1c1c3930b8501eb9e6e5bd0006e9f3
shahd_ahmed <shahdahmed8883@gmail.com>
2024-12-16 13:35:22 +0200

    finished

--- lib/welcome/welcome_screen 2.dart
+++ lib/welcome/welcome_screen 2.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:mini_coder/home/home_screen.dart';
 
@@ -8,7 +9,7 @@ class WelcomeScreen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2AAA8A),
+      backgroundColor: const Color(0xFF2AAA8A),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Stack(
@@ -16,6 +17,33 @@ class WelcomeScreen2 extends StatelessWidget {
             Positioned(
               child: Image.asset('assets/images/welcome_screen 1.png'),
             ),
+         const SizedBox(
+              height: 100,
+              width: 100,
+            ),
+           Padding(
+             padding: const EdgeInsets.all(80.0),
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.end,
+               children: [
+                const Text("Welcome",
+                   style: TextStyle(fontSize: 50,
+                       fontWeight:FontWeight.w600,color:Colors.white ),),
+              const SizedBox(
+                   height: 30,
+                 ),
+                 InkWell(
+                   onTap: (){
+                     Navigator.pushNamed(context,HomeScreen.routeName);
+                   },
+                   child:const Text("Tap   here  to  start",style:
+                  TextStyle(fontSize: 25,
+                       fontWeight: FontWeight.w400,color: Colors.white),),
+                 )
+               ],
+
+              ),
+           )
           ],
         ),
       ),
